981409cc270edc49356f14f1192276cd30dcff00
arifSDE <arifhosenb6@gmail.com>
2026-09-20 21:24:53 +0600

    My React First Project Setup

---
+++ src/App.tsx
@@ -0,0 +1,42 @@
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      
+      
+          <h1>Get started</h1>
+          <Student/>
+          <Persion/>
+          gadget
+          <Gedget/>
+
+    </>
+  )
+}
+
+function Student() {
+  return (
+    <div>
+      <h3>Name:</h3>
+      <p>Grades:</p>
+    </div>
+  )
+}
+
+function Persion() {
+  return <p>I am Here </p>
+}
+function Gedget() {
+  return (
+    <>
+      <p>Gadget</p>
+      <p>Some</p>
+      <p>Somein</p>
+    </>
+  )
+} 
+export default App
